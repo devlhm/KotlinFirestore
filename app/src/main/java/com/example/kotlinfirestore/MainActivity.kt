5dba9749674b48daf7d6e5771dc4c542b17a96e1
Luis H <devlhm05@gmail.com>
2022-09-19 09:10:21 -0300

    first commit

---
+++ app/src/main/java/com/example/kotlinfirestore/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.kotlinfirestore
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val submitButton = findViewById<Button>(R.id.registerSubmit);
+        submitButton.setOnClickListener { onSubmitButtonClick() };
+    }
+
+    private fun onSubmitButtonClick() {
+        FirebaseApp.initializeApp(this)
+
+        val name = findViewById<EditText>(R.id.editTextName).text.toString();
+        val adress = findViewById<EditText>(R.id.editTextAdress).text.toString();
+        val district = findViewById<EditText>(R.id.editTextDistrict).text.toString();
+        val cep = findViewById<EditText>(R.id.editTextCEP).text.toString();
+
+        val db = Firebase.firestore;
+
+        if(!validateFields(arrayOf(name, adress, district, cep))) {
+            return;
+        }
+
+        val person = hashMapOf(
+            "name" to name,
+            "address" to adress,
+            "district" to district,
+            "cep" to cep
+        )
+
+        db.collection("people")
+            .add(person)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+
+    public fun validateFields(fields: Array<String>): Boolean {
+        fields.forEach {
+            if(it == "") {
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_LONG);
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
